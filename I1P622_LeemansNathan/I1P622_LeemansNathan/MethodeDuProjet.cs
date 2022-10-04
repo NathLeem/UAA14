c4b93a354fa53fd6347c60875ef84703d059c3c8
Leemans Nathan <200253@CCM-Asty-Moulin.local>
2022-10-04 14:26:45 +0200

    interro POO

---
+++ I1P622_LeemansNathan/I1P622_LeemansNathan/MethodeDuProjet.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace I1P622_LeemansNathan
+{
+    public struct MethodeDuProjet
+    {
+        public void ChangerCouleur(int couleur, string identifiant, Random rnd, out string ResultCoul)
+        {
+            ResultCoul = "";
+            for (int i = 0; i < 4; i++)
+            {
+                couleur = rnd.Next(3);
+            }
+            if (couleur == 0)
+            {
+                ResultCoul = "Orange";
+            }
+            else if (couleur == 1)
+            {
+                ResultCoul = "Rouge";
+            }
+            else if (couleur == 2)
+            {
+                ResultCoul = "Vert";
+            }
+        }
+
+        public void StatutOnOff(bool onOff,out string ResultStatut)
+        {
+            ResultStatut = "";
+            for (int i = 0; i < 10; i++)
+            {
+                if (onOff == true)
+                {
+                    ResultStatut = "éteint";
+                    onOff = false;
+                }
+                else
+                {
+                    ResultStatut = "allumé";
+                    onOff = true;
+                }
+            }
+        }
+    }
+}
---
+++ I1P622_LeemansNathan/I1P622_LeemansNathan/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace I1P622_LeemansNathan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodeDuProjet MesOutils = new MethodeDuProjet();
+            string idCoul = "1900";
+            string idCligno = "011";
+            int couleur = 0;
+            bool onOff = true;
+            Random rnd = new Random();
+            string ResultCoul = "";
+            string ResultStatut = "";
+            int i = 0;
+            int j = 0;
+
+            Console.WriteLine("Etats de tout les feux");
+            MesOutils.ChangerCouleur(couleur, idCoul, rnd, out ResultCoul);
+            Console.WriteLine($"Le feu de signalisation {idCligno}  est {ResultCoul}");
+            MesOutils.ChangerCouleur(couleur, idCoul, rnd, out ResultCoul);
+            Console.WriteLine($"Le feu de signalisation {idCoul}  est {ResultCoul}");
+
+            Console.WriteLine("Changement des couleurs");
+            Console.WriteLine("-----------------------");
+            while (i != 5)
+            {
+                MesOutils.ChangerCouleur(couleur, idCoul, rnd, out ResultCoul);
+                Console.WriteLine($"Le feu de signalisation {idCoul}  est {ResultCoul}");
+                i++;
+            }
+
+            Console.WriteLine("Feu clignotant");
+            Console.WriteLine("--------------");
+            while (j != 10)
+            {
+                MesOutils.StatutOnOff(onOff,out ResultStatut);
+                Console.WriteLine($"{idCligno} est {ResultStatut}");
+                j++;
+            }
+        }
+    }
+}
